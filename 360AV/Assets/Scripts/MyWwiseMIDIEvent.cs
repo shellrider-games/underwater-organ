6fe9b97b52a833f404757c35de45ec62bdba81ef
Georg Becker <georg.becker@shellrider.com>
2025-03-28 14:28:04 +0100

    Adapt script for piano keys

--- 360AV/Assets/Scripts/MyWwiseMIDIEvent.cs
+++ 360AV/Assets/Scripts/MyWwiseMIDIEvent.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class MyWwiseMIDIEvent : MonoBehaviour
 {
     [SerializeField] private AK.Wwise.Event SynthEvent;
+    [SerializeField][Range(36,91)] private int note;
 
-    void Start()
+    public void PlayNote()
     {
         AkMIDIPostArray MIDIPostArrayBuffer = new AkMIDIPostArray(1);
 
         AkMIDIPost midiEvent = new AkMIDIPost();
         midiEvent.byType = AkMIDIEventTypes.NOTE_ON;
         midiEvent.byChan = 0;
-        midiEvent.byOnOffNote = 66;
+        midiEvent.byOnOffNote = (byte)note;
         midiEvent.byVelocity = 127;
         midiEvent.uOffset = 0;
 
         MIDIPostArrayBuffer[0] = midiEvent;
         SynthEvent.PostMIDI(gameObject, MIDIPostArrayBuffer);
     }
+
+    public void StopNote()
+    {
+        SynthEvent.StopMIDI(gameObject);
+    }
+    
     
 }
